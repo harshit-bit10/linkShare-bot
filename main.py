65fc4f75df0ce7fbb108c30b74a476e65cab6f9c
Harshit Papnai <109902163+harshit-bit10@users.noreply.github.com>
2026-05-29 14:19:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bot import Bot
 # ╔══════════════════════════════════════════════╗
-# ║         BotifyX_Pro_Botz — LinkShare Bot     ║
-# ║   Telegram: https://t.me/BotifyX_Pro_Botz    ║
+# ║         SharkToonsIndia — LinkShare Bot     ║
+# ║   Telegram: https://t.me/BSharkToonsIndia    ║
 # ╚══════════════════════════════════════════════╝
 Bot().run()
